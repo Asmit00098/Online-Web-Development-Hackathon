f64e6c1060ab6ef4024407f9a233bcd5e213ea6a
GulshanJha00 <gk4763549@gmail.com>
2024-04-07 18:35:39 +0530

    Final

--- Backend2/routes/index.js
+++ Backend2/routes/index.js
@@ -6,8 +6,14 @@ const localStrategy = require("passport-local")
 passport.use(new localStrategy(userModel.authenticate()))
 
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('landing');
 });
+
+router.get('/homepage', isLoggedIn ,function(req, res, next) {
+  res.render('homepage');
+});
+
+
 router.get('/login', function(req, res) {
   res.render('login');
 });
@@ -33,20 +39,20 @@ router.post("/register", function(req, res){
   userModel.register(userdata, req.body.password)
   .then(function (registereduser){
     passport.authenticate("local")(req,res,function(){
-      res.redirect("/")
+      res.redirect("/homepage")
     })
   })
 })
 
 router.post("/login", passport.authenticate("local",{
-  successRedirect: '/',
+  successRedirect: '/homepage',
   failureRedirect: "/login"
 }), function(req,res){ })
 
 router.get("/logout", function(req, res, next){
   req.logout(function(err){
     if(err) { return next(err);}
-    res.redirect("/")
+    res.redirect("/homepage")
   })
 })
 
